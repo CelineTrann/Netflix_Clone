da4d07fcfd5bda74f3907c809f339fb585e838fc
Celine Tran <celinephoungtran@gmail.com>
2023-08-21 15:28:43 -0400

    component not part of wrapper

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,9 +1,17 @@
+import { ArrowBackIosOutlined, ArrowForwardIosOutlined } from '@mui/icons-material';
 import './List.scss'
 
 export default function List() {
   return (
     <div className='list'>
-      List
+      <span className="listTitle">Continue to watch</span>
+      <div className="wrapper">
+        <ArrowBackIosOutlined />
+        <div className="container">
+          
+        </div>
+        <ArrowForwardIosOutlined />
+      </div>
     </div>
   );
 }
