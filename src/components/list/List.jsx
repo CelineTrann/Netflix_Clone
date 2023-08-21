cd2eca10c2b73ce9b1728aff8bd6acaf0a74a9a9
Celine Tran <celinephoungtran@gmail.com>
2023-08-21 16:14:45 -0400

    create carousel for list

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,14 +1,36 @@
 import { ArrowBackIosOutlined, ArrowForwardIosOutlined } from '@mui/icons-material';
 import ListItem from '../listItem/ListItem';
 import './List.scss'
+import { useRef, useState } from 'react';
 
 export default function List() {
+  const listRef = useRef();
+  const [slideNumber, setSlideNumber] = useState(0);
+
+  const handleClick = (direction) => {
+    let distance = listRef.current.getBoundingClientRect().x - 50;
+
+    if (direction === "left" && slideNumber > 0) {
+      setSlideNumber(slideNumber - 1);
+      listRef.current.style.transform = `translateX(${230 + distance}px)`;
+    } else if (direction === "right" && slideNumber < 5) {
+      setSlideNumber(slideNumber + 1)
+      listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+    }
+  };
+
   return (
     <div className='list'>
       <span className="listTitle">Continue to watch</span>
       <div className="wrapper">
-        <ArrowBackIosOutlined className="sliderArrow left" />
-        <div className="container">
+        <ArrowBackIosOutlined 
+          className="sliderArrow left" 
+          onClick={() => handleClick("left")} 
+          style={{display: (slideNumber === 0) && "none"}}
+        />
+        <div className="container" ref={listRef}>
+          <ListItem />
+          <ListItem />
           <ListItem />
           <ListItem />
           <ListItem />
@@ -19,7 +41,11 @@ export default function List() {
           <ListItem />
           <ListItem />
         </div>
-        <ArrowForwardIosOutlined className="sliderArrow right" />
+        <ArrowForwardIosOutlined 
+          className="sliderArrow right" 
+          onClick={() => handleClick("right")} 
+          style={{display: (slideNumber === 5) && "none"}}
+        />
       </div>
     </div>
   );
