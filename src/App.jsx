4ea11342a35fc251e401fe72f1b8efba50c120b8
Celine Tran <celinephoungtran@gmail.com>
2023-08-23 13:32:34 -0400

    initial scaffold based on register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.scss"
 import Home from './pages/home/Home'
+import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
 
 export default function App() {
   return (
-      <Register />
+      <Login />
   );
 }
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,23 @@
+import { useRef, useState } from 'react';
+import './Login.scss'
+
+const Login = () => {
+  return (
+    <div className="login">
+      <div className="top">
+        <div className="wrapper">
+          <img
+            className="logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="container">
+    
+      </div>
+    </div>
+  );
+}
+
+export default Login;
