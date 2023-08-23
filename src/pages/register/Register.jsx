612549be75b7dd8ade4b3f63dd85b03db1ee260b
Celine Tran <celinephoungtran@gmail.com>
2023-08-23 13:22:37 -0400

    fix state change

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -9,12 +9,12 @@ const Register = () => {
   const passwordRef = useRef();
 
   const handleStart = () => {
-    setEmail(emailRef.current.value)
-  }
+    setEmail(emailRef.current.value);
+  };
 
   const handleFinish = () => {
-    setPassword(passwordRef.current.value)
-  }
+    setPassword(passwordRef.current.value);
+  };
 
   return (
     <div className="register">
@@ -37,12 +37,16 @@ const Register = () => {
         {!email ? (
           <div className="input">
             <input type="email" placeholder="email address" ref={emailRef} />
-            <button className="registerButton" onClick={{ handleStart }} >Get Started</button>
+            <button className="registerButton" onClick={handleStart}>
+              Get Started
+            </button>
           </div>
         ) : (
           <form className="input">
             <input type="password" placeholder="password" ref={passwordRef} />
-            <button className="registerButton" onClick={{ handleFinish }}>Start Membership</button>
+            <button className="registerButton" onClick={handleFinish}>
+              Start
+            </button>
           </form>
         )}
       </div>
