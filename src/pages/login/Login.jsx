754c4fcb0d1864c5f8c4dd5fc56bd96ff97ad2db
Celine Tran <celinephoungtran@gmail.com>
2023-08-23 13:34:40 -0400

    add specific page elements

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -14,7 +14,17 @@ const Login = () => {
         </div>
       </div>
       <div className="container">
-    
+        <form >
+          <h1>Sign In</h1>
+          <input type="email" placeholder='email or phone number' />
+          <input type="password" placeholder='password' />
+          <button className="loginButton">Sign In</button>
+          <span>New to Netflix? <b>Sign up now.</b></span>
+          <small>
+            This page is protected by Google reCAPTCHA to ensure you're not a
+            bot. <b>Learn more</b>.
+          </small>
+        </form>
       </div>
     </div>
   );
